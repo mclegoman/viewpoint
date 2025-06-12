b5353bb33a7277fc01e760659b0bd1385cb3ded0
dannytaylor <daniel@mclegoman.com>
2025-06-12 21:31:45 +1200

    fixed panorama

--- src/main/java/com/mclegoman/viewpoint/client/panorama/ScreenshotRecorder.java
+++ src/main/java/com/mclegoman/viewpoint/client/panorama/ScreenshotRecorder.java
@@ -7,12 +7,26 @@
 
 package com.mclegoman.viewpoint.client.panorama;
 
+import com.mclegoman.viewpoint.common.data.Data;
+import com.mclegoman.viewpoint.luminance.common.util.LogType;
 import net.minecraft.client.gl.Framebuffer;
+import net.minecraft.util.Util;
 
 import java.io.File;
 
 public class ScreenshotRecorder {
-	public static void saveScreenshot(File gameDirectory, String fileName, Framebuffer framebuffer) {
-		net.minecraft.client.util.ScreenshotRecorder.saveScreenshot(gameDirectory, fileName, framebuffer, (text) -> {});
+	public static void saveScreenshot(File directory, String fileName, Framebuffer framebuffer) {
+		net.minecraft.client.util.ScreenshotRecorder.takeScreenshot(framebuffer, (image) -> {
+			directory.mkdir();
+			File file = new File(directory, fileName);
+			Util.getIoWorkerExecutor().execute(() -> {
+				try {
+					image.writeTo(file);
+					image.close();
+				} catch (Exception exception) {
+					Data.getVersion().sendToLog(LogType.ERROR, "Couldn't save screenshot" + exception);
+				}
+			});
+		});
 	}
 }
